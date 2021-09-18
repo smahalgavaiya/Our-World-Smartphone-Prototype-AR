81de01b90041b6c7497f98d982e248a0fdc28e5d
Yiğitcan Öksüz <yigitcanoksuz@gmail.com>
2021-09-18 23:18:22 +0300

    CrossfadeTransition script added for loading ui

---
+++ Assets/_Game/Scripts/Controllers/GameController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+            CrossfadeTransition.Instance.FadeIn(null);
+        if (Input.GetKeyDown(KeyCode.P))
+            CrossfadeTransition.Instance.FadeOut(null);
+    }
+#endif
+}
