1d125f62aa130560819058655ea052007cb7dd1c
Yiğitcan Öksüz <yigitcanoksuz@gmail.com>
2021-09-25 20:59:07 +0300

    data mapper created for converting information from MapBox.Feature to ParkData

--- Assets/_Game/Scripts/DataModels/ParkData.cs
+++ Assets/_Game/Scripts/DataModels/ParkData.cs
@@ -5,7 +5,13 @@ namespace OurWorld.Scripts.DataModels
     public class ParkData
     {
         public readonly string Name;
-
         public readonly Geolocation Geolocation;
+        public float Distance = 0f;
+
+        public ParkData(string name, Geolocation geolocation)
+        {
+            Name = name;
+            Geolocation = geolocation;
+        }
     }
 }
