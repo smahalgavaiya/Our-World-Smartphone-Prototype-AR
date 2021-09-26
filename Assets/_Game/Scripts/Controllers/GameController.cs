19eb9bcc8cd183e48b72667757ac2ef6ce379094
Yiğitcan Öksüz <yigitcanoksuz@gmail.com>
2021-09-26 16:06:03 +0300

    helper methods for initialization added to GameController class

--- Assets/_Game/Scripts/Controllers/GameController.cs
+++ Assets/_Game/Scripts/Controllers/GameController.cs
@@ -1,20 +1,50 @@
+using System;
+using Cysharp.Threading.Tasks;
 using OurWorld.Scripts.DataModels.GeolocationData;
 using OurWorld.Scripts.Extensions;
+using OurWorld.Scripts.Interfaces.MapAPI;
 using OurWorld.Scripts.Providers.MapAPIProviders;
 using OurWorld.Scripts.Utilities;
 using UnityEngine;
 namespace OurWorld.Scripts.Controllers
 {
-
     public class GameController : MonoBehaviour
     {
-
-        private async void Awake()
+        public static event Action GameInitialized;
+        private IMapAPIProvidder _mapApiProvider;
+        private bool _initialized;
+        public bool Initialized => _initialized;
+        public IMapAPIProvidder MapAPIProvidder => _mapApiProvider;
+        private void Awake()
         {
-           var apiProvider = new MapboxAPIProvider();
-
-           await apiProvider.GetNearbyParksAsync(new Geolocation(32.707270,39.995767),1f);
+            Initialize();
         }
+
+        private async void Initialize()
+        {
+            _mapApiProvider = new MapboxAPIProvider();
+
+            await _mapApiProvider.GetNearbyParksAsync(new Geolocation(32.707270, 39.995767), 1f);
+
+            _initialized = true;
+
+            GameInitialized?.Invoke();
+        }
+
+        public void DoAfterInitialize(Action callBack)
+        {
+            if (Initialized) callBack?.Invoke();
+
+            GameInitialized += OnInitialize;
+
+            void OnInitialize()
+            {
+                GameInitialized -= OnInitialize;
+                callBack?.Invoke();
+            }
+        }
+
+
 #if UNITY_EDITOR
         private void Update()
         {
