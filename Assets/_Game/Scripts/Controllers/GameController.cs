7e4572d9efbe7f838da842d176ad50d1b52adaf3
Yiğitcan Öksüz <yigitcanoksuz@gmail.com>
2021-10-11 20:40:07 +0300

    Unused namespaces removed.

--- Assets/_Game/Scripts/Controllers/GameController.cs
+++ Assets/_Game/Scripts/Controllers/GameController.cs
@@ -1,7 +1,4 @@
 using System;
-using Cysharp.Threading.Tasks;
-using OurWorld.Scripts.DataModels.GeolocationData;
-using OurWorld.Scripts.Extensions;
 using OurWorld.Scripts.Interfaces.MapAPI;
 using OurWorld.Scripts.Providers.MapAPIProviders;
 using OurWorld.Scripts.Utilities;
