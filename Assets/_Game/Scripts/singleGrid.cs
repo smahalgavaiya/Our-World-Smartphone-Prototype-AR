3e575cd625db3f47e05221b57544c57f91715cf7
smahalgavaiya <36693350+smahalgavaiya@users.noreply.github.com>
2021-10-17 20:36:42 +0530

    added land buy in map scene

--- Assets/_Game/Scripts/singleGrid.cs
+++ Assets/_Game/Scripts/singleGrid.cs
@@ -1,30 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class singleGrid : MonoBehaviour
 {
-    MeshRenderer gridMat;
+    //MeshRenderer gridMat;
+    Image singleGridImage;
 
     Color translucent = new Color(1, 1, 1, 0.2f);
     Color transparent= new Color(1, 1, 1, 0.45f);
 
     private void Start()
     {
-        gridMat = GetComponent<MeshRenderer>();
-        gridMat.material.color = translucent;
+        singleGridImage = GetComponent<Image>();
+        singleGridImage.color = translucent;
+        
+     /*   gridMat = GetComponent<MeshRenderer>();
+        gridMat.material.color = translucent;*/
     }
 
     public void Gridselected()
     {
-        gridMat.material.color = transparent;
+        // gridMat.material.color = transparent;
+        singleGridImage.color = transparent;
     }
     public void Gridunselected()
     {
-        gridMat.material.color = translucent;
+        // gridMat.material.color = translucent;
+        singleGridImage.color = translucent;
     }
 
     public void checkGrid()
     {
-        if (gridMat.material.color == transparent)
+        // if (gridMat.material.color == transparent)
+        if (singleGridImage.color == transparent)
             Gridunselected();
         else
             Gridselected();
