c5a1efaea483d9092813def5969d01236d255a35
smahalgavaiya <36693350+smahalgavaiya@users.noreply.github.com>
2023-01-13 17:32:09 +0530

    Added missing script

---
+++ Assets/_Game/Scripts/Avatar Info/LevelProgressUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine ;
+using UnityEngine.UI ;
+
+public class LevelProgressUI : MonoBehaviour {
+
+   [Header ("UI references :")]
+   [SerializeField] private Image uiFillImage ;
+   [SerializeField] private Text uiStartText ;
+   [SerializeField] private Text uiEndText ;
+
+    float currentKarmaValue = .5f;
+
+
+   private void Start () {
+      
+   }
+
+
+   public void SetLevelTexts (int currentKarmaPoints) {
+      uiStartText.text = "L"+Mathf.FloorToInt(PlayerPrefs.GetInt("KarmaPoints") / 100f).ToString () ;
+        int pointsNeedeForLevelUp = 100-(currentKarmaPoints % 100);
+      uiEndText.text = pointsNeedeForLevelUp+"+";
+   }
+
+    private IEnumerator fillAnim(float duration)
+    {
+        float time = 0.0f;
+        while (time < duration)
+        {
+            uiFillImage.fillAmount = time / duration;
+            time += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        uiFillImage.fillAmount = currentKarmaValue;
+    }
+
+   public void UpdateProgressFill (float value) {
+      currentKarmaValue = value;
+      uiFillImage.fillAmount = value ;
+        //play animation only when we level up
+      string karmaPrefString = "karmaLevelAnim";
+        if (!PlayerPrefs.HasKey(karmaPrefString))
+        {
+            StartCoroutine(fillAnim(2.5f));
+            PlayerPrefs.SetString(karmaPrefString, uiStartText.text);
+        }
+        else 
+        {
+            if (PlayerPrefs.GetString(karmaPrefString) != uiStartText.text)
+                StartCoroutine(fillAnim(2.5f));
+
+            PlayerPrefs.SetString(karmaPrefString, uiStartText.text);
+        }
+      
+    }
+}
